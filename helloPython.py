d77508b21fd7a5f27537773f9ba2bfa97568d35f
ryukenpa <ryukenpa@gmail.com>
2017-05-13 13:09:11 -0400

    add files via uploading

---
+++ helloPython.py
@@ -0,0 +1,16 @@
+import random
+import sys
+import os
+import math
+
+print('A')
+print('B')
+
+
+person = input('Enter your name: ')
+print('Hello', person)
+z = input('How are you today?')
+if z == 'good':
+    print(z, 'glad to hear that!')
+else:
+    print(z, '!!! sorry to hear that!')
